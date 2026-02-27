a028d22ae93e4d23b2a93f3912f2653a6b985499
kartik-cloud148 <itsyashofficial504@gmail.com>
2026-02-27 16:47:03 +0530

    ec2 updated for terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "ec2cloud" {
+resource "aws_instance" "demo-2" {
     ami = "ami-0b6c6ebed2801a5cb"
     instance_type = "t3.micro"
 
