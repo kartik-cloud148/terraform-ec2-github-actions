380b1d54c978510fa9c4d47768c3e5b962e39d6d
kartik-cloud148 <itsyashofficial504@gmail.com>
2026-02-27 16:19:48 +0530

    ec2 updates

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2cloud" {
     ami = "ami-0b6c6ebed2801a5cb"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
 
     tags = {
         NAME= "ec2terraformgithub"
